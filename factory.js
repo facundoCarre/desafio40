ac69aba6e680ed4fe5ce6e041b2a21bdd9bb1dfd
Facundo Carreño <fcarreno@simtlix.com>
2021-09-24 19:50:28 -0300

    se agrega el singleton

--- factory.js
+++ factory.js
@@ -1,14 +1,20 @@
+let instance = null
+
 class PersistenciaFactory {
 
     constructor() { }
 
     getPersistencia(tipo) {
-        try {
-            let modulo = require(`./persistencia/${tipo}`);
-            return modulo
-        } catch (error) {
-            console.log('No se encontro el tipo de persistencia:');
+        if(!instance){
+            try {
+                instance = require(`./persistencia/${tipo}`);
+                //return modulo
+            } catch (error) {
+                console.log('No se encontro el tipo de persistencia:');
+            }
+            instance = new instance()
         }
+        return instance
     }
 }
 
