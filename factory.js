1381d37814bd58107394d66b6c8615f1a4417764
Facundo Carreño <fcarreno@simtlix.com>
2021-09-28 22:25:35 -0300

    arreglos

--- factory.js
+++ factory.js
@@ -1,20 +1,14 @@
-let instance = null
-
 class PersistenciaFactory {
 
     constructor() { }
 
     getPersistencia(tipo) {
-        if(!instance){
-            try {
-                instance = require(`./persistencia/${tipo}`);
-                //return modulo
-            } catch (error) {
-                console.log('No se encontro el tipo de persistencia:');
-            }
-            instance = new instance()
+        try {
+            let modulo = require(`./persistencia/${tipo}`);
+            return modulo
+        } catch (error) {
+            console.log('No se encontro el tipo de persistencia:');
         }
-        return instance
     }
 }
 
